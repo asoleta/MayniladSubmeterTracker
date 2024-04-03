812200a77ead83558e13576d6f34e7da6b131121
Alex Soleta <alexandrasoleta@gmail.com>
2024-04-03 10:35:25 -0400

    Implement the View Data functionality

--- MayniladSubmeterTracker/filterDataResults.cs
+++ MayniladSubmeterTracker/filterDataResults.cs
@@ -30,11 +30,18 @@ namespace MayniladSubmeterTracker
                 //open the connection
                 connection.Open();
                 
-                // SQL query to retrieve month and year from searchQuery table
+                // SQL query to retrieve month and year from searchQueries table
                 string sqlQuery = "SELECT TOP 1 [month], [year] FROM searchQueries ORDER BY id DESC";
+                string queryBillTotal = $"SELECT * FROM submeterReading WHERE month = @Month AND year = @Year";
+                string querySub1a = "SELECT * FROM submeter1A WHERE month = @Month AND year = @Year;";
+                string querySub2a = "SELECT * FROM submeter2A WHERE month = @Month AND year = @Year;";
+                string querySub2b = "SELECT * FROM submeter2B WHERE month = @Month AND year = @Year;";
+                string querySub3a = "SELECT * FROM submeter3A WHERE month = @Month AND year = @Year;";
+                string querySub3b = "SELECT * FROM submeter3B WHERE month = @Month AND year = @Year;";
 
                 using (SqlCommand command = new SqlCommand(sqlQuery, connection))
                 {
+                    // Gets the desired month and year from the searchQueries table
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         // Check if there are rows returned
@@ -49,9 +56,174 @@ namespace MayniladSubmeterTracker
                             Console.WriteLine("No rows found in searchQuery table.");
                         }
                     }
+                }
 
-                    //close the connection
-                    connection.Close();
+                //Submeter 1a
+                using (SqlCommand command = new SqlCommand(querySub1a, connection))
+                {
+                    // Add parameters to the SqlCommand
+                    command.Parameters.AddWithValue("@Month", monthSearch);
+                    command.Parameters.AddWithValue("@Year", yearSearch);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Access the columns by name and store them in variables
+                            //month = reader.GetInt32(reader.GetOrdinal("month"));
+                            //year = reader.GetInt32(reader.GetOrdinal("year"));
+                            waterUsage = reader.GetInt32(reader.GetOrdinal("waterUsage"));
+                            cost = Convert.ToDouble(reader["costPerCubic"]);
+                            amtDue = Convert.ToDouble(reader["amtDue"]);
+
+                            //Display the information in the textboxes
+                            //dateTB.Text = month.ToString() + "/" + year.ToString();
+                            unit1waterTB.Text = waterUsage.ToString();
+                            unit1CostTB.Text = cost.ToString("0.00") + "P";
+                            unit1TotalTB.Text = amtDue.ToString("0.00") + "P";
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found.");
+                        }
+                    }
+                }
+
+                //Submeter 2a
+                using (SqlCommand command = new SqlCommand(querySub2a, connection))
+                {
+                    // Add parameters to the SqlCommand
+                    command.Parameters.AddWithValue("@Month", monthSearch);
+                    command.Parameters.AddWithValue("@Year", yearSearch);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Access the columns by name and store them in variables
+                            waterUsage = reader.GetInt32(reader.GetOrdinal("waterUsage"));
+                            cost = Convert.ToDouble(reader["costPerCubic"]);
+                            amtDue = Convert.ToDouble(reader["amtDue"]);
+
+                            //Display the information in the textboxes
+                            unit2aWaterTB.Text = waterUsage.ToString();
+                            unit2aCostTB.Text = cost.ToString("0.00") + "P";
+                            unit2aTotalTB.Text = amtDue.ToString("0.00") + "P";
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found.");
+                        }
+                    }
+                }
+
+                //Submeter 2b
+                using (SqlCommand command = new SqlCommand(querySub2b, connection))
+                {
+                    // Add parameters to the SqlCommand
+                    command.Parameters.AddWithValue("@Month", monthSearch);
+                    command.Parameters.AddWithValue("@Year", yearSearch);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Access the columns by name and store them in variables
+                            waterUsage = reader.GetInt32(reader.GetOrdinal("waterUsage"));
+                            cost = Convert.ToDouble(reader["costPerCubic"]);
+                            amtDue = Convert.ToDouble(reader["amtDue"]);
+
+                            //Display the information in the textboxes
+                            unit2bWaterTB.Text = waterUsage.ToString();
+                            unit2bCostTB.Text = cost.ToString("0.00") + "P";
+                            unit2bTotalTB.Text = amtDue.ToString("0.00") + "P";
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found.");
+                        }
+                    }
+                }
+
+                //Submeter 3a
+                using (SqlCommand command = new SqlCommand(querySub3a, connection))
+                {
+                    // Add parameters to the SqlCommand
+                    command.Parameters.AddWithValue("@Month", monthSearch);
+                    command.Parameters.AddWithValue("@Year", yearSearch);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Access the columns by name and store them in variables
+                            waterUsage = reader.GetInt32(reader.GetOrdinal("waterUsage"));
+                            cost = Convert.ToDouble(reader["costPerCubic"]);
+                            amtDue = Convert.ToDouble(reader["amtDue"]);
+
+                            //Display the information in the textboxes
+                            unit3aWaterTB.Text = waterUsage.ToString();
+                            unit3aCostTB.Text = cost.ToString("0.00") + "P";
+                            unit3aTotalTB.Text = amtDue.ToString("0.00") + "P";
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found.");
+                        }
+                    }
+                }
+
+                //Submeter 3b
+                using (SqlCommand command = new SqlCommand(querySub3b, connection))
+                {
+
+                    // Add parameters to the SqlCommand
+                    command.Parameters.AddWithValue("@Month", monthSearch);
+                    command.Parameters.AddWithValue("@Year", yearSearch);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Access the columns by name and store them in variables
+                            waterUsage = reader.GetInt32(reader.GetOrdinal("waterUsage"));
+                            cost = Convert.ToDouble(reader["costPerCubic"]);
+                            amtDue = Convert.ToDouble(reader["amtDue"]);
+
+                            //Display the information in the textboxes
+                            unit3bWaterTB.Text = waterUsage.ToString();
+                            unit3bCostTB.Text = cost.ToString("0.00") + "P";
+                            unit3bTotalTB.Text = amtDue.ToString("0.00") + "P";
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found.");
+                        }
+                    }
+                }
+
+                //Total Bill
+                using (SqlCommand command = new SqlCommand(queryBillTotal, connection))
+                {
+                    // Add parameters to the SqlCommand (prevent SQL injection)
+                    command.Parameters.AddWithValue("@Month", monthSearch);
+                    command.Parameters.AddWithValue("@Year", yearSearch);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Access the columns by name and store them in variables
+                            billTotal = Convert.ToDouble(reader["billTotal"]);
+
+                            //Display the information in the textboxes
+                            totalBillTB.Text = billTotal.ToString("0.00") + "P";
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found.");
+                        }
+                    }
                 }
             }
 
