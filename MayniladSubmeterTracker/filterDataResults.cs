931e80df8d27f0bc747f668bd1bfa8f23789ede3
Alex Soleta <alexandrasoleta@gmail.com>
2024-04-03 09:39:40 -0400

    Connect the filter data results page to the searchQueries table.

--- MayniladSubmeterTracker/filterDataResults.cs
+++ MayniladSubmeterTracker/filterDataResults.cs
@@ -16,6 +16,46 @@ namespace MayniladSubmeterTracker
         public filterDataResults()
         {
             InitializeComponent();
+
+            //declare variables
+            int monthSearch = 0;
+            int yearSearch = 0;
+            int waterUsage;
+            double cost = 0.0;
+            double amtDue = 0.0;
+            double billTotal = 0.0;
+
+            using (SqlConnection connection = new SqlConnection("Data Source=LAPTOP-EF4ATSUG\\SQLEXPRESS01;Initial Catalog=Maynilad;Integrated Security=True;TrustServerCertificate=True"))
+            {
+                //open the connection
+                connection.Open();
+                
+                // SQL query to retrieve month and year from searchQuery table
+                string sqlQuery = "SELECT TOP 1 [month], [year] FROM searchQueries ORDER BY id DESC";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Check if there are rows returned
+                        if (reader.Read())
+                        {
+                            // Assign month and year values from the query result
+                            monthSearch = Convert.ToInt32(reader["month"]);
+                            yearSearch = Convert.ToInt32(reader["year"]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found in searchQuery table.");
+                        }
+                    }
+
+                    //close the connection
+                    connection.Close();
+                }
+            }
+
+            MessageBox.Show(monthSearch.ToString() + " " + yearSearch.ToString());
         }
 
         //the close button stops and closes the application 
